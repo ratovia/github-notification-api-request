f5514e9928c980dc75973f31d7faef5ca8c92cae
ratovia <ratovia@hotmail.co.jp>
2021-08-15 06:07:43 +0900

    fix rubocop

--- app/services/github/base.rb
+++ app/services/github/base.rb
@@ -1,14 +1,12 @@
 module Github
   class Base
-    BASE_URL = 'https://api.github.com'
-    API_VERSION = 'application/vnd.github.v3+json'
-    attr_accessor :token, :base_uri
+    BASE_URL = 'https://api.github.com'.freeze
+    API_VERSION = 'application/vnd.github.v3+json'.freeze
 
-    def initialize()
-      @token = token
+    def initialize
       @base_uri = BASE_URL
       @base_headers = {
-        Accept: "#{API_VERSION}"
+        Accept: API_VERSION
       }
       @client = Faraday.new @base_uri do |f|
         f.request  :url_encoded
@@ -21,20 +19,5 @@ module Github
     def get(path, params = {}, options = {})
       @client.get(path, params, options)
     end
-
-    # def post(path, options = {})
-    #   options[:headers] = { 'Authorization' => "token #{@token}" }
-    #   self.class.post(path, options)
-    # end
-
-    # def put(path, options = {})
-    #   options[:headers] = { 'Authorization' => "token #{@token}" }
-    #   self.class.put(path, options)
-    # end
-
-    # def delete(path, options = {})
-    #   options[:headers] = { 'Authorization' => "token #{@token}" }
-    #   self.class.delete(path, options)
-    # end
   end
 end
